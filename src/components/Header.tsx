3177958669253de8014b096f7c00e8be05fa81cb
noonnoo <kdy24610@gmail.com>
2022-08-24 01:43:23 +0900

    feature: Header 화면 개발

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,160 @@
 import KurlyLogo from "@/assets/kurly_logo.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import HeartIcon from "@/assets/icons/HeartIcon";
+import styled from "styled-components";
+import { IUserInfo } from "@/modules/UserModule";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const HeaderWrapper = styled.div`
+  display: block;
+  width: 1050px;
+  padding: 0 10px;
+  margin: auto;
+`;
+
+const SectionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  &.gnb {
+    justify-content: space-evenly;
+  }
+`;
+
+const HedaerUser = styled(Link)`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: var(--text-first);
+  letter-spacing: -0.3px;
+  text-decoration: none;
+  margin-left: auto;
+  cursor: pointer;
+
+  span {
+    display: inline-block;
+    min-width: 38px;
+    height: 14px;
+    margin-right: 4px;
+    padding: 0px 4px;
+    border-radius: 30px;
+    vertical-align: 0px;
+    font-size: 10px;
+    color: var(--primary-color);
+    letter-spacing: -0.3px;
+    text-align: center;
+    border: 1px solid var(--primary-color);
+    line-height: 14px;
+    background-color: white;
+  }
+
+  p {
+    margin: 0 0 0 4px;
+  }
+`;
+
+const HeaderLogo = styled(Link)`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 42px;
+    cursor: pointer;
+  }
+`;
+
+const HeaderTitle = styled(Link)`
+  min-width: 80px;
+  font-weight: 500;
+  font-size: 18px;
+  text-align: center;
+  color: var(--primary-color);
+  margin-left: 20px;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const HeartIconWrapper = styled.span`
+  margin-left: auto;
+  height: 27px;
+  width: 27px;
+`;
+
+const HeaderGNBMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 150px;
+  height: 55px;
+  padding-top: 18px;
+  line-height: 20px;
+  text-align: center;
+`;
+
+const HeaderGNBText = styled(Link)`
+  height: 20px;
+  font-size: 16px;
+  font-weight: 500;
+  color: var(--text-first);
+  cursor: pointer;
+  letter-spacing: -0.3px;
+  text-decoration: none;
+
+  &:hover {
+    color: var(--primary-color);
+    text-decoration-line: underline;
+    text-decoration-thickness: 1px;
+    text-underline-position: under;
+  }
+`;
 
 export default function Header() {
+  const user: IUserInfo = {
+    name: "김컬리",
+    grade: "더 퍼플",
+    id: null,
+    description: null,
+    isSuper: false,
+  };
+
+  const globalNavigation = [
+    { label: "컬리 추천", key: "main", link: "/main" },
+    { label: "장바구니 무료공개", key: "cart", link: "/goods-list" },
+  ];
+
   return (
-    <div className="Header">
-      <img className="HeaderLogo" src={KurlyLogo} alt="마켓컬리 로고"></img>
-      <h3>마켓컬리</h3>
-      <p>
-        <FontAwesomeIcon icon={faHeart} />
-      </p>
-    </div>
+    <Container>
+      <HeaderWrapper>
+        <SectionWrapper>
+          <HedaerUser to="/mypage">
+            <span>{user.grade}</span>
+            <p>{user.name} 님</p>
+          </HedaerUser>
+        </SectionWrapper>
+        <SectionWrapper>
+          <HeaderLogo to="/main">
+            <img src={KurlyLogo} alt="마켓컬리 로고"></img>
+          </HeaderLogo>
+          <HeaderTitle to="/main">마켓컬리</HeaderTitle>
+          <HeartIconWrapper>
+            <HeartIcon
+              fill="var(--light-background)"
+              stroke="var(--text-first)"
+            />
+          </HeartIconWrapper>
+        </SectionWrapper>
+        <SectionWrapper className="gnb">
+          {globalNavigation.map((item) => {
+            return (
+              <HeaderGNBMenu key={item.key}>
+                <HeaderGNBText to={item.link}>{item.label}</HeaderGNBText>
+              </HeaderGNBMenu>
+            );
+          })}
+        </SectionWrapper>
+      </HeaderWrapper>
+    </Container>
   );
 }
---
+++ src/modules/UserModule.ts
@@ -0,0 +1,7 @@
+export interface IUserInfo {
+  id: string | null;
+  name: string | null;
+  grade: string | null;
+  description: string | null;
+  isSuper: boolean | false;
+}
