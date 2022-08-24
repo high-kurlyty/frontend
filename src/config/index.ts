7bcac6e94c4605b670d5f04cb25fd8ec34cb1b52
noonnoo <kdy24610@gmail.com>
2022-08-24 17:03:17 +0900

    fix: 사용자 fetch API 반영

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 const Config = {
   REACT_APP_BACKEND_HOST: "http://43.200.27.212:3000/api",
+  USER_ID: "kyukyu",
 };
 
 export default Config;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,9 +3,14 @@ import axios from "axios";
 
 export default {
   getUser() {
+    const config = {
+      params: {
+        id: Config.USER_ID,
+      },
+    };
     return axios
-      .get(Config.REACT_APP_BACKEND_HOST + "/user")
-      .then((res) => res.data[0])
+      .get(Config.REACT_APP_BACKEND_HOST + "/user/profile", config)
+      .then((res) => res.data)
       .catch((err) => {
         console.log(err);
       });
